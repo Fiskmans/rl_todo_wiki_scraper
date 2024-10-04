a637f5e0928aa88e9ad3da3ad475b2505ab47861
Måns Zivkovic Andersen <mans.andersen@massive.se>
2024-10-04 13:54:46 +0200

    Add warning to big categories

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ import api
 
 import items
 import quests
+import os
 
 api.use_cache = True
 
 itemNameToId = items.run()
 quests.run(itemNameToId)
 
+def ClumpWarnings(path: str):
+    files = 0
+    for node in os.listdir(path):
+        nodePath = os.path.join(path, node)
+        if os.path.isfile(nodePath):
+            files += 1
+        elif os.path.isdir(nodePath):
+            ClumpWarnings(nodePath)
+    
+    if files > 30:
+        print("Warning {} has {} methods in it, consider splitting it up".format(path, files))
+
+ClumpWarnings("Baked")
